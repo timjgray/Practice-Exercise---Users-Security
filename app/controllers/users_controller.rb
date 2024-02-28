531caa5ea6ad2f58d0361b0d53612dc592c07366
Tim Gray <tim.gray@kellogg.northwestern.edu>
2024-02-28 22:45:30 +0000

    practice exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
     # TODO: encrypt user's password "at rest"
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["password"])
     @user.save
     redirect_to "/posts"
   end
